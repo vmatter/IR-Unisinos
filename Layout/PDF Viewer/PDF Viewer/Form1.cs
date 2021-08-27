c34cd75c3f9fe68a0c275b056e458498e951ffd8
vmatter <vmatter@edu.unisinos.br>
2021-08-26 23:10:17 -0300

    Solved the Split bug.

--- Layout/PDF Viewer/PDF Viewer/Form1.cs
+++ Layout/PDF Viewer/PDF Viewer/Form1.cs
@@ -52,6 +52,14 @@ namespace PDF_Viewer
             {
                 axAcroPDF1.src = openFileDialog.FileName;
                 documentName = openFileDialog.FileName;
+
+                string teste = documentName;
+
+
+                string[] names = teste.Split('\\');
+                //this.textBox2.Text = names.Length.ToString();
+                this.textBox2.Text = names[names.Length -1];
+
             }
             else
             {
