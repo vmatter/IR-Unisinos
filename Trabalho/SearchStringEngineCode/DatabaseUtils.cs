934b37d42c5db4b43dcbbb5150fcc0854028d208
JrBackes <junior.backes@hotmail.com.br>
2021-09-04 10:16:14 -0300

    Adding WordValidation and SaveIntoBD

--- Trabalho/SearchStringEngineCode/DatabaseUtils.cs
+++ Trabalho/SearchStringEngineCode/DatabaseUtils.cs
@@ -5,6 +5,7 @@ using System.Data.SQLite;
 
 namespace SearchStringHandler
 {
+
     public static class DatabaseUtils
     {
         #region Connection
@@ -17,16 +18,59 @@ namespace SearchStringHandler
             sqliteConnection.Open();
 
         }
+
         // Use SQLite site to create tables and 
         private static void CreateDB (string nome){
             SQLiteConnection.CreateFile(nome);
+            using (var cmd = sqliteConnection.CreateComand())
+            {
+                cmd.CommandText = "CREATE TABLE IF NOT EXISTS FileData(File Varchar(40), SearchWord Varchar(30), NumberRep int)"
+
+            }
+        }
+
+        // Execute just one time;
+        // private static void CreateTable ()
+        // {
+        //     using (var cmd = sqliteConnection.CreateComand())
+        //     {
+        //         cmd.CommandText = "CREATE TABLE IF NOT EXISTS FileData(File Varchar(40), SearchWord Varchar(30), NumberRep int)"
+
+        //     }
+        // }
+
+        private static void WordValidation(string FName, string SWord)
+        {
+            //TODO: Verify SQL return, and do a IF condition to prevent duplicate words.
+            using (var cmd = sqliteConnection.CreateCommand())
+            {
+                cmd.CommandText = "SELECT COUNT(*) FROM FileData WHERE File LIKE '@FName' AND SearchWord LIKE '@SWord';"
+                cmd.Parameters.AddWithValue(@FName,Fname);
+                cmd.Parameters.AddWithValue(@SWord,SWord);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private static void SaveIntoBD(string FName, string SWord, int NRep)
+        {
+            using (var cmd = sqliteConnection.CreateCommand())
+            {
+                cmd.CommandText = "INSERT INTO FileData(File, SearchWord, NumberRep) values (@FName, @SWord, @NRep);"
+
+                cmd.Parameters.AddWithValue(@FName,Fname);
+                cmd.Parameters.AddWithValue(@SWord,SWord);
+                cmd.Parameters.AddWithValue(@NRep,NRep);
+                cmd.ExecuteNonQuery();
+            }
         }
 
         private static void ReadDB()
         {
             using (var cmd = new System.Data.SQLite.SQLiteCommand(sqliteConnection))
             {
-                
+                cmd.CommandText = "SELECT File, SearchWord, NumberRep from FileData;"
+
+
             }
 
         }
