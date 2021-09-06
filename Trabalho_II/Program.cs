fd09e68b1f22becb7fdc436b9120c3828d00b11f
vmatter <vmatter@edu.unisinos.br>
2021-09-06 13:58:22 -0300

    Changed the directories names.

---
+++ Trabalho_II/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trabalho_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
